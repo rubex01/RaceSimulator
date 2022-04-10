122dead0239bf902d8911261602ba549b7b2b9a3
rubex01 <40766757+rubex01@users.noreply.github.com>
2022-04-10 21:17:18 +0200

    Level 6-3

---
+++ RaceSimulatorGUI/GenerateImages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RaceSimulatorGUI
+{
+    public static class GenerateImages
+    {
+        private static Dictionary<string, Bitmap>
+    }
+}
